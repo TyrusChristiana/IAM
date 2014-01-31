1c9f4b91e66df7e42d40d911d96253edbbd28ea3
TyrusChristiana <tyrus@tyrus.net>
2014-01-30 22:28:53 -0500

    Update server.js
    Added comment

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var connect = require('connect'), http = require('http');
 var shoutOut = function() {
   console.log("I AM  at your service on port: " + myport);
 }
+//Set the NITROUS.IO Default Workspace directory as the root to serve static files from.
 var app = connect().use(connect.static(__dirname + '/workspace'));
 //Run
 app.listen(myport);
